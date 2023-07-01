b1c3c46b13b31a073da2705d8883f248a4ad4cac
anhaaa2305 <anhaaa2305@gmail.com>
2023-07-01 22:48:30 +0700

    Completed UISearchBarViewController

--- Todoey/Controller/ToDoListViewController.swift
+++ Todoey/Controller/ToDoListViewController.swift
@@ -8,32 +8,33 @@ class ToDoListViewController: UITableViewController {
     // MARK: - Global Variales
     var itemArray = [Item]()
     //let defaults = UserDefaults.standard        // Set defaults with Standard   , we don't use UserDefaults => Comment defaults
-    
-    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext // UIApplication.shared.delegate as! AppDelegate to call AppDelegate
+    //let request: NSFetchRequest<Item> = Item.fetchRequest()
 
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext // UIApplication.shared.delegate as! AppDelegate to call AppDelegate
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
         // Do any additional setup after loading the view.
         /*
-        let newItem = Item()
-        newItem.title = "Find Mike"
-        itemArray.append(newItem)
-        let newItem2 = Item()
-        newItem2.title = "Buy Oggs"
-        itemArray.append(newItem2)
-        let newItem3 = Item()
-        newItem3.title = "Destroy Demogorgon"
-        itemArray.append(newItem3)
+         let newItem = Item()
+         newItem.title = "Find Mike"
+         itemArray.append(newItem)
+         let newItem2 = Item()
+         newItem2.title = "Buy Oggs"
+         itemArray.append(newItem2)
+         let newItem3 = Item()
+         newItem3.title = "Destroy Demogorgon"
+         itemArray.append(newItem3)
          */
         /*
-        if let items = defaults.array(forKey: "toDoListArray") as? [Item] {     // Call data in
-            itemArray = items
-        }
-        */
+         if let items = defaults.array(forKey: "toDoListArray") as? [Item] {     // Call data in
+         itemArray = items
+         }
+         */
         
-        //DecoderData()
+        loadItem()
     }
     
     // MARK: - Table DataSource Methods
@@ -48,30 +49,37 @@ class ToDoListViewController: UITableViewController {
         
         // Set the Checkmark
         /*
-        if itemArray[indexPath.row].done == true {
-            cell.accessoryType = .checkmark
-        } else {
-            cell.accessoryType = .none
-        }
-        */
+         if itemArray[indexPath.row].done == true {
+         cell.accessoryType = .checkmark
+         } else {
+         cell.accessoryType = .none
+         }
+         */
         cell.accessoryType = itemArray[indexPath.row].done ? .checkmark : .none    // Change for the line 44 to 49
-        return cell 
+        return cell
     }
-  
+    
     // MARK: - Table View Delegate Method
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         //print(itemArray[indexPath.row]) // get text
         
         /*
-        if itemArray[indexPath.row].done == false {
-            itemArray[indexPath.row].done = true
-        } else {
-            itemArray[indexPath.row].done = false
-        }
-        */
-        itemArray[indexPath.row].done = !itemArray[indexPath.row].done   // Change for the line 57 to 62
+         if itemArray[indexPath.row].done == false {
+         itemArray[indexPath.row].done = true
+         } else {
+         itemArray[indexPath.row].done = false
+         }
+         */
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done // Change for the line 57 to 62
         tableView.reloadData()       // Reload Data in Delegate Method to DataSource
         tableView.deselectRow(at: indexPath, animated: true)
+        
+        
+        // Delete Item:
+        /*
+        context.delete(itemArray[indexPath.row])
+        itemArray.remove(at: indexPath.row)
+         */
     }
     
     // MARK: - Add Items Button:
@@ -84,48 +92,62 @@ class ToDoListViewController: UITableViewController {
             newItem.title = textField.text!
             newItem.done = false
             self.itemArray.append(newItem)
-            self.EncoderData()
+            self.createItem()
         }
         // Add textField
         alert.addTextField { alertTextField in
             alertTextField.placeholder = "Create new Item"
-                textField = alertTextField
+            textField = alertTextField
         }
         // Add action on the Screen
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
     }
-    // MARK: Codable Data
-    // Encoder Data into Todoey_Item.plist
-    func EncoderData() {
+    // MARK: Refresh button
+    @IBAction func refreshButton(_ sender: UIBarButtonItem) {
+        
+    }
+    // MARK: CRUD Data
+    // Create Data into Todoey_Item.plist
+    func createItem() {
         do {
             /* We don't use Encoder , because of we comments this line
-            let dataEncoder = try encoder.encode(itemArray)   // Encoder data in itemArray
-            try dataEncoder.write(to: dataFilePath!)         // Write to Todoey_Item.plist
+             let dataEncoder = try encoder.encode(itemArray)   // Encoder data in itemArray
+             try dataEncoder.write(to: dataFilePath!)         // Write to Todoey_Item.plist
              */
             try context.save()
             print("Completed save data")
         } catch {
             //print("Error encoding item array \(error)")
-            print("Error saving data: \(error)")
+            print("Error saving data into Context: \(error)")
         }
         self.tableView.reloadData()
     }
-    /*
-    // Decoder Data from Todoey_Item.plist
-    func DecoderData() {
-        if let dataDecoder = try? Data(contentsOf: dataFilePath!) {
-            let decoder = PropertyListDecoder()
-            do {
-                /* We don't use Decoder, because of we comments this line
-                itemArray = try decoder.decode([Item].self, from: dataDecoder)
-                 */
-                
-            } catch {
-                //print("Error Decoding \(error)")
-            }
+    // Read Data from Todoey_Item.plist        // LoadITem
+    func loadItem(with request: NSFetchRequest<Item> = Item.fetchRequest()) {
+        do {
+            itemArray = try context.fetch(request)
+        } catch {
+            print("Error fetching data from Context: \(error)")
+        }
+        self.tableView.reloadData()
+    }
+}
+extension ToDoListViewController: UISearchBarDelegate,UIImagePickerControllerDelegate,UIPickerViewDelegate {
+    // MARK: Setting for Search bar setting
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        let request: NSFetchRequest<Item> = Item.fetchRequest()
+        request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        request.sortDescriptors  = [NSSortDescriptor(key: "title", ascending: true)]
+        loadItem(with: request)
+    }
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0 {
+            loadItem()
+        }
+        DispatchQueue.main.async {
+            searchBar.resignFirstResponder()
         }
     }
-     */
 }
 
